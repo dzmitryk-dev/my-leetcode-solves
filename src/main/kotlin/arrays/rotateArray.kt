acf330f5f208b571140b5c07fd6da0b511a8ae9d
dzmitry <koirn.dev@gmail.com>
2021-02-08 16:07:36 +0300

    Solve of https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/

---
+++ src/main/kotlin/arrays/rotateArray.kt
@@ -0,0 +1,30 @@
+package arrays
+
+fun rotate(nums: IntArray, k: Int) {
+    val size = nums.size
+    val realK = k % size
+    if (size == 1 || realK == 0) {
+        return
+    }
+    nums.reverse()
+    nums.reverse(0, realK)
+    nums.reverse(realK, size)
+}
+
+fun IntArray.reverse(fromIndex: Int, toIndex: Int): Unit {
+    if (fromIndex < 0 || toIndex > size) {
+        throw IndexOutOfBoundsException("fromIndex: $fromIndex, toIndex: $toIndex, size: $size")
+    }
+    if (fromIndex > toIndex) {
+        throw IllegalArgumentException("fromIndex: $fromIndex > toIndex: $toIndex")
+    }
+    val midPoint = (fromIndex + toIndex) / 2
+    if (fromIndex == midPoint) return
+    var reverseIndex = toIndex - 1
+    for (index in fromIndex until midPoint) {
+        val tmp = this[index]
+        this[index] = this[reverseIndex]
+        this[reverseIndex] = tmp
+        reverseIndex--
+    }
+}
---
+++ src/test/kotlin/arrays/RotateArrayTest.kt
@@ -0,0 +1,74 @@
+package arrays
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class RotateArrayTest {
+
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    fun test(testData: TestData) {
+        val data = testData.data.clone()
+        val k = testData.k
+        val expected = testData.expected
+        rotate(data, k)
+        Assertions.assertArrayEquals(
+            expected,
+            data,
+            "rotate for ${testData.data.joinToString(",", "[", "]")} should be ${expected.joinToString(",", "[", "]")} but " +
+                    "was ${data.joinToString(",", "[", "]")} instead"
+        )
+    }
+
+    companion object {
+
+        data class TestData(
+            val data: IntArray,
+            val k: Int,
+            val expected: IntArray
+        ) {
+            override fun toString(): String {
+                return "input = ${data.joinToString(",", "[", "]")}, k = $k, expected = ${expected.joinToString(",", "[", "]")}"
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as TestData
+
+                if (!data.contentEquals(other.data)) return false
+                if (k != other.k) return false
+                if (!expected.contentEquals(other.expected)) return false
+
+                return true
+            }
+
+            override fun hashCode(): Int {
+                var result = data.contentHashCode()
+                result = 31 * result + k
+                result = 31 * result + expected.contentHashCode()
+                return result
+            }
+        }
+
+        @JvmStatic
+        fun provideTestData() = listOf(
+            TestData(intArrayOf(1,2,3,4,5,6,7), 3, intArrayOf(5,6,7,1,2,3,4)),
+            TestData(intArrayOf(-1,-100,3,99), 2, intArrayOf(3,99,-1,-100)),
+            TestData(intArrayOf(1), 0, intArrayOf(1)),
+            TestData(intArrayOf(1), 1, intArrayOf(1)),
+            TestData(intArrayOf(1,2,3,4,5,6,7), 0, intArrayOf(1,2,3,4,5,6,7)),
+            TestData(intArrayOf(1,2,3,4,5,6,7), 1, intArrayOf(7,1,2,3,4,5,6)),
+            TestData(intArrayOf(1,2), 3, intArrayOf(2, 1)),
+            TestData(intArrayOf(1,2), 2, intArrayOf(1, 2)),
+            TestData(intArrayOf(1,2,3,4,5,6), 4, intArrayOf(3,4,5,6,1,2)),
+            TestData(intArrayOf(1,2,3,4,5,6, 7), 4, intArrayOf(4,5,6,7,1,2,3)),
+            TestData(
+                intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27),
+                38,
+                intArrayOf(17,18,19,20,21,22,23,24,25,26,27,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)),
+        )
+    }
+}
