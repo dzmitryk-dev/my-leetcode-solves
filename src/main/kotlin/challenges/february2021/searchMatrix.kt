8bc8416af10fab619237fc8eef84c1e109ccdf58
dzmitry <koirn.dev@gmail.com>
2021-02-27 23:40:24 +0300

    Solve of https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3650/

--- src/main/kotlin/challenges/february2021/searchMatrix.kt
+++ src/main/kotlin/challenges/february2021/searchMatrix.kt
@@ -1,26 +1,33 @@
 package challenges.february2021
 
+
+/**
+ * https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3650/
+ */
 fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
     val m = matrix.lastIndex
     val n = matrix[0].lastIndex
     var row = 0
-    var col = 0
+    var col = n
 
-    tailrec fun search(matrix: Array<IntArray>, target: Int, row: Int, col: Int, dy: Int = 0, dx: Int = 1): Boolean {
-        if (row > m) {
-
+    while (col >= 0 && row <= m) {
+        if (matrix[row][col] == target) return true
+        while (col >= 0 && row <= m) {
+            val v = matrix[row][col]
+            if (v < target) {
+                row++
+            } else {
+                break
+            }
         }
-        if (col > n) {
-
-        }
-        val v = matrix[row][col]
-        if (v == target) return true
-        return if (v > target) {
-            search(matrix, target, row, col + 1)
-        } else {
-            search(matrix, target, row + 1, col - 1)
+        while (col >= 0 && row <= m) {
+            val v = matrix[row][col]
+            if (v > target) {
+                col--
+            } else {
+                break
+            }
         }
     }
     return false
 }
-
--- src/test/kotlin/challenges/february2021/SearchMatrixTest.kt
+++ src/test/kotlin/challenges/february2021/SearchMatrixTest.kt
@@ -49,6 +49,19 @@ class SearchMatrixTest {
                     intArrayOf(10,13,14,17,24),
                     intArrayOf(18,21,23,26,30),
                 ), 20, false),
+            // matrix = [[-5]] , target = -5, true
+            TestData(
+                arrayOf(
+                    intArrayOf(-5),
+                ), -5, true),
+            TestData(
+                arrayOf(
+                    intArrayOf(1,4,7,11,15),
+                    intArrayOf(2,5,8,12,19),
+                    intArrayOf(3,6,9,16,22),
+                    intArrayOf(10,13,14,17,24),
+                    intArrayOf(18,21,23,26,30),
+                ), 18, true),
         )
     }
 }
