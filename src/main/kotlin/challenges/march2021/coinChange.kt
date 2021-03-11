b5398aa8fb6c378b3792455ce00983a67ac5e4ad
dzmitry <koirn.dev@gmail.com>
2021-03-11 21:38:51 +0300

    Solve of https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3668/

--- src/main/kotlin/challenges/march2021/coinChange.kt
+++ src/main/kotlin/challenges/march2021/coinChange.kt
@@ -1,23 +1,37 @@
 package challenges.march2021
 
+import java.util.*
+
 /*
     https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3668/
  */
 fun coinChange(coins: IntArray, amount: Int): Int {
     if (amount == 0) return 0
-    var counter = 0
-    var amountRest = amount
+
     coins.sortDescending()
-    for (nominal in coins) {
-        val result = amountRest / nominal
-        if (result > 0) {
-            counter += result
-            amountRest %= nominal
+    return bfs(sortedSetOf(amount), coins)
+}
+
+private tailrec fun bfs(target: SortedSet<Int>, candidates: IntArray, step: Int = 0): Int {
+    if (target.isContainsZero()) return step
+    if (target.isEmpty()) return -1
+    val newTargets = candidates
+        .flatMap { nominal ->
+            target.map { t ->
+                t - nominal
+            }
+        }.filterNot { it < 0 }
+        .toSortedSet()
+    return bfs(newTargets, candidates, step + 1)
+}
+
+private fun SortedSet<Int>.isContainsZero(): Boolean {
+    for (i in this) {
+        if (i == 0) {
+            return true
+        } else if (i > 0) {
+            return false
         }
     }
-    return if (amountRest > 0) {
-        -1
-    } else {
-        counter
-    }
+    return false
 }
