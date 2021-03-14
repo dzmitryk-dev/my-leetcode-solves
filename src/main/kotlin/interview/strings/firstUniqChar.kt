48e8103518d79d9122d18e5a6a359c80cb7873fe
dzmitry <koirn.dev@gmail.com>
2021-03-14 22:06:47 +0300

    Solve of https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/

--- src/main/kotlin/interview/strings/firstUniqChar.kt
+++ src/main/kotlin/interview/strings/firstUniqChar.kt
@@ -1,6 +1,19 @@
 package interview.strings
 
+/*
+  https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/881/
+ */
+
 fun firstUniqChar(s: String): Int {
-//    s.groupBy({})
-    return 0
+    if (s.isEmpty()) return -1
+    val map = LinkedHashMap<Char, IntArray>()
+    s.forEachIndexed { index, c ->
+        map.compute(c) { _, old->
+            old?.let { intArrayOf(*old, index) } ?: intArrayOf(index)
+        }
+    }
+    return Iterable { map.iterator() }
+        .firstOrNull { (_,arr) -> arr.size == 1 }
+        ?.let { (_, arr) -> arr[0] }
+        ?: -1
 }
