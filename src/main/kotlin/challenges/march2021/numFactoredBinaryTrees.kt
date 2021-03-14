ca86ee7b05425a88ea9fab0fc3917a352e8560f5
dzmitry <koirn.dev@gmail.com>
2021-03-14 11:34:09 +0300

    Solve of https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3670/

--- src/main/kotlin/challenges/march2021/numFactoredBinaryTrees.kt
+++ src/main/kotlin/challenges/march2021/numFactoredBinaryTrees.kt
@@ -1,37 +1,40 @@
 package challenges.march2021
 
-import java.util.HashMap
-
-import kotlin.math.sqrt
-
 /*
     https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3670/
  */
+/*
+  My solution
+ */
+fun numFactoredBinaryTrees(arr: IntArray): Int {
+    arr.sort()
+    val map = mutableMapOf<Int, Long>()
+    val s = arr.toSet()
+    for ((index1, value1 ) in arr.withIndex()) {
+        var ways1 = map.getOrPut(value1) { 1 }
+        if (index1 > 0) {
+            for (index2 in index1 - 1 downTo 0) {
+                val value2 = arr[index2]
+                val ways2 = map.getOrPut(value2) { 1 }
+                if (value1 % value2 == 0) {
+                    val division = value1 / value2
+                    if (s.contains(division)) {
+                        val waysDivision = map.getOrDefault(division, 1)
+                        ways1 += (ways2 * waysDivision)
+                    }
+                }
+            }
+            map[value1] = ways1
+        }
+    }
+    return ((map.values.fold(0L) { acc, l -> acc + l }) % 1000000007).toInt()
+}
 
-//fun numFactoredBinaryTrees(arr: IntArray): Int {
-//    arr.sort()
-//    val map = mutableMapOf<Int, Long>()
-//    val s = arr.toSet()
-//    for ((index1, value1 ) in arr.withIndex()) {
-//        val ways1 = map.getOrPut(value1) { 1 }
-//        for (index2 in index1..arr.lastIndex) {
-//            val value2 = arr[index2]
-//            val ways2 = map.getOrPut(value2) { 1 }
-//            val multiplication = value1 * value2
-//            if (s.contains(multiplication)) {
-//                var w = map.getOrDefault(multiplication, 1)
-//                w += (ways2 * ways1) * if (value1 != value2) {
-//                    2
-//                } else {
-//                    1
-//                }
-//                map[multiplication] = w
-//            }
-//        }
-//    }
-//    return ((map.values.fold(0L) { acc, l -> acc + l }) % 1000000007).toInt()
-//}
 
+/*
+ * Solution from leetcode forum
+ */
+/*
 fun numFactoredBinaryTrees(arr: IntArray): Int {
     arr.sort()
     var ans: Long = 0
@@ -54,4 +57,4 @@ fun numFactoredBinaryTrees(arr: IntArray): Int {
         ans = (ans + ways) % 1000000007
     }
     return ans.toInt()
-}
+}*/
--- src/test/kotlin/challenges/march2021/NumFactoredBinaryTreesTest.kt
+++ src/test/kotlin/challenges/march2021/NumFactoredBinaryTreesTest.kt
@@ -36,15 +36,15 @@ class NumFactoredBinaryTreesTest {
                 Output: 3
                 Explanation: We can make these trees: [2], [4], [4, 2, 2]
                  */
-//                TestData(intArrayOf(2, 4), 3),
+                TestData(intArrayOf(2, 4), 3),
                 /*
                 Example 2:
                 Input: arr = [2,4,5,10]
                 Output: 7
                 Explanation: We can make these trees: [2], [4], [5], [10], [4, 2, 2], [10, 2, 5], [10, 5, 2].
                  */
-//                TestData(intArrayOf(2, 4, 5, 10), 7),
-//                TestData(intArrayOf(18,3,6,2), 12),
+                TestData(intArrayOf(2, 4, 5, 10), 7),
+                TestData(intArrayOf(18,3,6,2), 12),
                 TestData(intArrayOf(45,42,2,18,23,1170,12,41,40,9,47,24,33,28,10,32,29,17,46,11,759,37,6,26,21,49,31,14,19,8,13,7,27,22,3,36,34,38,39,30,43,15,4,16,35,25,20,44,5,48), 777)
             )
     }
