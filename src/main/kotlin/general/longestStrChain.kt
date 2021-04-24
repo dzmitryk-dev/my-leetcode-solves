17124cc1f60c0817a2127f5a1dd7168684e9ad66
dzmitry <koirn.dev@gmail.com>
2021-04-24 19:00:03 +0300

    Solve of https://leetcode.com/problems/longest-string-chain/

--- src/main/kotlin/general/longestStrChain.kt
+++ src/main/kotlin/general/longestStrChain.kt
@@ -1,3 +1,5 @@
+package general
+
 /**
  * https://leetcode.com/problems/longest-string-chain/
  */
--- src/test/kotlin/general/LongestStrChainTest.kt
+++ src/test/kotlin/general/LongestStrChainTest.kt
@@ -1,6 +1,5 @@
 package general
 
-import longestStrChain
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
